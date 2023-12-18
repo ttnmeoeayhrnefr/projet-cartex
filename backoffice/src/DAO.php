65ad98868ec9ae95ffd8af25f47524812aac7271
Rémi KORZENIOWSKI <r.korzeniowski@icloud.com>
2023-12-18 15:58:41 +0100

    ci: DAO.php creation

--- backoffice/src/DAO.php
+++ backoffice/src/DAO.php
@@ -1,3 +1,16 @@
 <?php
-    
+    require '/Applications/XAMPP/xamppfiles/htdocs/projet-cartex/backoffice/src/config.php';
+    try {
+        echo "TRUE";
+    } catch (PDOException $e) {
+        echo "WRONG" . $e->getMessage();
+    }
+    class Cards {
+        // CRUD card 
+    }
+    class Utilisateurs {
+        // if user admin ?
+        // CRUD user 
+    }
+    class DAO {}
 ?>
