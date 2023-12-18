0e2a10df6a368c478d7278b1ab22fd9d11caa042
Rémi KORZENIOWSKI <r.korzeniowski@icloud.com>
2023-12-18 12:03:07 +0100

    ci: PDF added

---
+++ backoffice/panel.php
@@ -0,0 +1,21 @@
+<div class="page-sct">
+    <div class="navbar-sct">
+        <div class="logo-sct">
+
+        </div>
+        <div class="btn-sct">
+
+        </div>
+        <div class="search-sct">
+            
+        </div>
+    </div>
+    <div class="panel-sct">
+        <div class="card-sct">
+
+        </div>
+        <div class="user-sct">
+            
+        </div>
+    </div>
+</div>
