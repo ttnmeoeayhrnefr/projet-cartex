f00e34824fff81d819305b55ba68c5264386277d
RaphTPLR <102389757+RaphTPLR@users.noreply.github.com>
2023-12-19 11:12:34 +0100

    feat: cards design

--- front/src/views/marketplace.jsx
+++ front/src/views/marketplace.jsx
@@ -1,11 +1,35 @@
 import '../style/marketplace.scss'
 
 export default function MarketPlace() {
+    const generateCardList = () => {
+        const cardList = [];
+    
+        for (let i = 0; i < 10; i++) {
+          cardList.push(
+            <div className="card" key={i}>
+              <div className="title">
+                <span>Title</span>
+                <p>#152354751255</p>
+              </div>
+              <div className="img">
+                <img src="https://i.pinimg.com/564x/2d/e0/b7/2de0b7946bb7c8427018694ce5464709.jpg" alt="" />
+              </div>
+              <div className="describe">
+                <div className="price">
+                  <span>1.99$</span>
+                </div>
+              </div>
+            </div>
+          );
+        }
+    
+        return cardList;
+      };
     return (
         <div className="content-mkp">
             <div className="header">
                 <h2>Bienvenue sur CarteX,</h2>
-                <p>Trouve les cartes que tu désires !</p>
+                <p>Trouve toutes les cartes que tu désires !</p>
             </div>
             <div className="filterbar">
                 <div className="filters">
@@ -17,6 +41,9 @@ export default function MarketPlace() {
                 <div className="searchbar">
                     <input type="text" placeholder='Rechercher...'/>
                 </div>
+            </div>
+            <div className="container-cards-mkt">
+                {generateCardList()}
             </div>
         </div>
     )
