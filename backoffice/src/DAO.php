1d3e89bd5684e95f64cceaca85f137fedbbd224d
Rémi KORZENIOWSKI <r.korzeniowski@icloud.com>
2023-12-20 16:40:20 +0100

    fix: fatal errors

--- backoffice/src/DAO.php
+++ backoffice/src/DAO.php
@@ -220,6 +220,17 @@ class Utilisateurs
 
     public function __construct($id, $pseudo, $password, $role)
     {
+        if (empty($pseudo)) {
+            throw new InvalidArgumentException("Le pseudo de l'utilisateur est requis.");
+        }
+
+        if (empty($password)) {
+            throw new InvalidArgumentException("Le mot de passe de l'utilisateur est requis.");
+        }
+
+        if ($role !== '0' && $role !== '1') {
+            throw new InvalidArgumentException("Le rôle de l'utilisateur doit être soit 0 soit 1.");
+        }
         $this->id = $id;
         $this->pseudo = $pseudo;
         $this->password = $password;
@@ -349,6 +360,42 @@ class DAO
 
 
         public function addCard($nom,$image,$image_small,$image_cropped,$id_konami,$description,$type,$race,$attack,$defense,$stars,$archetype,$attribute,$cardmarket_price,$ebay_price,$amazon_price,$tcgplayer_price,$collection,$rarete) {
+            if ($type === 'monstre') {
+                $requiredMonsterType = [
+                    'race' => $race,
+                    'attack' => $attack,
+                    'defense' => $defense,
+                    'stars' => $stars,
+                    'attribute' => $attribute
+                ];
+    
+                foreach ($requiredMonsterType as $field => $value) {
+                    if (empty($value)) {
+                        throw new InvalidArgumentException("Le champ '$field' est obligatoire pour une carte de type 'monstre'.");
+                    }
+                }
+            }
+            $required = [
+                'nom' => $nom,
+                'image' => $image,
+                'image_small' => $image_small,
+                'image_cropped' => $image_cropped,
+                'id_konami' => $id_konami,
+                'description' => $description,
+                'type' => $type,
+                'cardmarket_price' => $cardmarket_price,
+                'tcgplayer_price' => $tcgplayer_price,
+                'ebay_price' => $ebay_price,
+                'amazon_price' => $amazon_price,
+                'collection' => $collection,
+                'rarete' => $rarete
+            ];
+    
+            foreach ($required as $field => $value) {
+                if (empty($value)) {
+                    throw new InvalidArgumentException("Le champ '$field' de la carte est requis.");
+                }
+            }
             try {
                 $row = $this->bdd->prepare("INSERT INTO carte(nom, image, image_petite, image_cropped, id_carte_konami, description, type, race, attaque, defense, etoiles, archetype,
                 attribut, cardmarket_price, ebay_price, amazon_price, tcgplayer_price, set_nom, set_rarete) VALUES (:nom, :image, :image_small, :image_cropped, :id_konami, :description, :type, 
