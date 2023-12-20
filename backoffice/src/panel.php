f14b4285ed2410f57d7e3eb600b9ad424229e863
ttnmeoeayhrnefr <theo.menantferry@outlook.com>
2023-12-20 11:48:19 +0100

    test: adapt from xamp to wamp

--- backoffice/src/panel.php
+++ backoffice/src/panel.php
@@ -1,35 +1,38 @@
 <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Connexion -  AdminPanel</title>
-        <link rel="stylesheet" href="style.scss">
-    </head>
-    <body>
-        <div class="page-sct">
-            <div class="navbar-sct">
-                <div class="logo-sct">
-                    <h1>Panneau d'administration</h1>
-                </div>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion - AdminPanel</title>
+    <link rel="stylesheet" href="style.scss">
+</head>
+
+<body>
+    <div class="page-sct">
+        <div class="navbar-sct">
+            <div class="logo-sct">
+                <h1>Panneau d'administration</h1>
             </div>
-            <div class="panel-sct">
-                <div class="log-sct">
-                    <div class="input-sct">
-                        <div class="user-sct">
-                            <label for="logUsername">Nom d'utilisateur</label>
-                            <input type="text" name="logUsername" id="logUser">
-                        </div>
-                        <div class="pwd-sct">
-                            <label for="logPasswd">Mot de passe</label>
-                            <input type="text" name="logPasswd" id="logPwd">
-                        </div>
+        </div>
+        <div class="panel-sct">
+            <div class="log-sct">
+                <div class="input-sct">
+                    <div class="user-sct">
+                        <label for="logUsername">Nom d'utilisateur</label>
+                        <input type="text" name="logUsername" id="logUser">
                     </div>
-                    <div class="submit-sct">
-                        <input type="submit" value="Connexion">
+                    <div class="pwd-sct">
+                        <label for="logPasswd">Mot de passe</label>
+                        <input type="text" name="logPasswd" id="logPwd">
                     </div>
                 </div>
+                <div class="submit-sct">
+                    <input type="submit" value="Connexion">
+                </div>
             </div>
-        </div>  
-    </body>
+        </div>
+    </div>
+</body>
+
 </html>
--- backoffice/src/request.php
+++ backoffice/src/request.php
@@ -1,21 +1,20 @@
 <?php
-    include "/Applications/XAMPP/xamppfiles/htdocs/projet-cartex/backoffice/src/config.php";
-    include "/Applications/XAMPP/xamppfiles/htdocs/projet-cartex/backoffice/src/DAO.php";
-    
-    $DAO = new DAO($connexion);
+include "./config.php";
+include "./DAO.php";
 
-    $userHome = "/Applications/XAMPP/xamppfiles/htdocs/projet-cartex/backoffice/src/user_panel.php";
-    
-    switch ($_POST['userSelection']) {
-        case 1:
-            $user = $DAO->addUser($_POST['userPseudo'],$_POST['userMdp'],$_POST['userMdp']);
-            header('location: ' . $userHome);
-            break;
-        case 2:
-            break;
-        case 3:
-            break;
-        case 4;
-            break;
-    }
-?>
+$DAO = new DAO($connexion);
+
+$userHome = "./user_panel.php";
+
+switch ($_POST['userSelection']) {
+    case 1:
+        $user = $DAO->addUser($_POST['userPseudo'], $_POST['userMdp'], $_POST['userMdp']);
+        header('location: ' . $userHome);
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4;
+        break;
+}
