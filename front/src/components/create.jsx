fa2b94eaf18ba2b469a697ca3c68d55c6661316f
Purplezer <a.do@ecole-ipssi.net>
2023-12-21 19:57:10 +0100

    Fix: Fix des inputs image et de l'ajout des image dans la BDD

--- front/src/components/create.jsx
+++ front/src/components/create.jsx
@@ -6,23 +6,23 @@ import { useEffect } from 'react';
 // Composant pour afficher une étoile
 const StarImage = ({ filled }) => {
     const starImageUrl = filled
-      ? 'https://lauqerm.github.io/ygocarder/asset/image/sub-family/subfamily-level.png' // Image d'étoile remplie
-      : '';
-  
+        ? 'https://lauqerm.github.io/ygocarder/asset/image/sub-family/subfamily-level.png' // Image d'étoile remplie
+        : '';
+
     return <img src={starImageUrl} alt="Star" className="star-image" />;
 };
 
 // Mappe les types aux URLs des images correspondantes
 const typeImageMap = {
-  monstre: 'https://lauqerm.github.io/ygocarder/asset/image/frame/frame-normal.png',
-  monstre_effet: 'https://lauqerm.github.io/ygocarder/asset/image/frame/frame-effect.png',
-  magie: 'https://lauqerm.github.io/ygocarder/asset/image/frame/frame-spell.png',
-  piege: 'https://lauqerm.github.io/ygocarder/asset/image/frame/frame-trap.png',
-  fusion: 'https://discord.com/channels/@me/1186238206912499782/1187343187879137300',
-  link: 'https://lauqerm.github.io/ygocarder/asset/image/frame/frame-fusion.png',
-  ritual: 'https://lauqerm.github.io/ygocarder/asset/image/frame/frame-ritual.png',
-  synchro: 'https://lauqerm.github.io/ygocarder/asset/image/frame/frame-synchro.png',
-  xyz: 'https://lauqerm.github.io/ygocarder/asset/image/frame/frame-xyz.png',
+    monstre: 'https://lauqerm.github.io/ygocarder/asset/image/frame/frame-normal.png',
+    monstre_effet: 'https://lauqerm.github.io/ygocarder/asset/image/frame/frame-effect.png',
+    magie: 'https://lauqerm.github.io/ygocarder/asset/image/frame/frame-spell.png',
+    piege: 'https://lauqerm.github.io/ygocarder/asset/image/frame/frame-trap.png',
+    fusion: 'https://discord.com/channels/@me/1186238206912499782/1187343187879137300',
+    link: 'https://lauqerm.github.io/ygocarder/asset/image/frame/frame-fusion.png',
+    ritual: 'https://lauqerm.github.io/ygocarder/asset/image/frame/frame-ritual.png',
+    synchro: 'https://lauqerm.github.io/ygocarder/asset/image/frame/frame-synchro.png',
+    xyz: 'https://lauqerm.github.io/ygocarder/asset/image/frame/frame-xyz.png',
 };
 
 export default function Create() {
@@ -38,71 +38,71 @@ export default function Create() {
     const [cardAmazonPrice, setCardAmazonPrice] = useState('');
     const [cardAttack, setCardAttack] = useState('');
     const [cardDefense, setCardDefense] = useState('');
-    const [cardImage, setCardImage] = useState(null);
-    const [userId, setUserId] = useState("");
+    const [cardImage, setCardImage] = useState('');
+    const [userId, setUserId] = useState('');
 
     useEffect(() => {
-      const fetchUserId = () => {
-        const idUserFromLocalStorage = localStorage.getItem("id_user");
-        setUserId(idUserFromLocalStorage);
-      };
-  
-      fetchUserId();
-    }, []); 
-  
+        const fetchUserId = () => {
+            const idUserFromLocalStorage = localStorage.getItem('id_user');
+            setUserId(idUserFromLocalStorage);
+        };
+
+        fetchUserId();
+    }, []);
+
     const handleNameChange = (event) => {
-      const newName = event.target.value;
-      setCardName(newName);
+        const newName = event.target.value;
+        setCardName(newName);
     };
-  
+
     const handleStarsChange = (event) => {
-      const newStars = event.target.value;
-      setCardStars(newStars);
+        const newStars = event.target.value;
+        setCardStars(newStars);
     };
 
     const handleTypeChange = (event) => {
-      const newType = event.target.value;
-      setCardType(newType);
+        const newType = event.target.value;
+        setCardType(newType);
     };
-  
+
     const handleCollectionChange = (event) => {
-      const newCollection = event.target.value;
-      setCardCollection(newCollection);
+        const newCollection = event.target.value;
+        setCardCollection(newCollection);
     };
-  
+
     const handleRarityChange = (event) => {
-      const newRarity = event.target.value;
-      setCardRarity(newRarity);
+        const newRarity = event.target.value;
+        setCardRarity(newRarity);
     };
-  
+
     const handleDescriptionChange = (event) => {
-      const newDescription = event.target.value;
-      setCardDescription(newDescription);
+        const newDescription = event.target.value;
+        setCardDescription(newDescription);
     };
-  
+
     const handleCmktPriceChange = (event) => {
-      const newCmktPrice = event.target.value;
-      setCardCmktPrice(newCmktPrice);
+        const newCmktPrice = event.target.value;
+        setCardCmktPrice(newCmktPrice);
     };
-  
+
     const handleTcgPriceChange = (event) => {
-      const newTcgPrice = event.target.value;
-      setCardTcgPrice(newTcgPrice);
+        const newTcgPrice = event.target.value;
+        setCardTcgPrice(newTcgPrice);
     };
-  
+
     const handleEbayPriceChange = (event) => {
-      const newEbayPrice = event.target.value;
-      setCardEbayPrice(newEbayPrice);
+        const newEbayPrice = event.target.value;
+        setCardEbayPrice(newEbayPrice);
     };
-  
+
     const handleAmazonPriceChange = (event) => {
-      const newAmazonPrice = event.target.value;
-      setCardAmazonPrice(newAmazonPrice);
+        const newAmazonPrice = event.target.value;
+        setCardAmazonPrice(newAmazonPrice);
     };
-    
+
     const handleAttackChange = (event) => {
-      const newAttack = event.target.value;
-      setCardAttack(newAttack);
+        const newAttack = event.target.value;
+        setCardAttack(newAttack);
     };
 
     const handleDefenseChange = (event) => {
@@ -111,105 +111,133 @@ export default function Create() {
     };
 
     const handleImageChange = (event) => {
-      const selectedImage = event.target.files[0];
-      setCardImage(selectedImage);
+        const imageUrl = event.target.value;
+        setCardImage(imageUrl);
     };
-    
-    const handleSubmit = async () => {
-      console.log(cardAttack, cardDefense, cardAmazonPrice, cardCmktPrice, cardCollection, cardDescription, cardEbayPrice, cardImage, cardName, cardRarity, cardStars, cardTcgPrice, cardType, userId)
-      const requestData = {
-        nom: cardName,
-        type: cardType,
-        description: cardDescription,
-        image: cardImage ? cardImage.name : '',
-        image_cropped: '',
-        image_petite: '',
-        id_carte_konami: null,
-        attaque: cardAttack,
-        defense: cardDefense,
-        etoiles: cardStars,
-        attribut: '',
-        cardmarket_price: cardCmktPrice,
-        tcgplayer_price: cardTcgPrice,
-        ebay_price: cardEbayPrice,
-        amazon_price: cardAmazonPrice,
-        set_nom: cardCollection,
-        set_rarete: '',
-        id_user: userId,
-      };
 
-      console.log(requestData.attaque, requestData.defense, requestData.amazon_price, requestData.cardmarket_price, requestData.set_nom, requestData.description, requestData.ebay_price, requestData.image, requestData.nom, requestData.set_rarete, requestData.etoiles, requestData.tcgplayer_price, requestData.type, requestData.id_user);
-  
-      resetForm();
-      try {
-        const response = await axios.post('http://localhost:3001/cartes', requestData);
-        console.log(response.data);
-        // Gérez le succès, redirigez ou affichez un message de réussite
-      } catch (error) {
-        console.error(error);
-        // Gérez l'erreur, affichez un message d'erreur
-      }
+    const handleSubmit = async () => {
+        console.log(
+            cardAttack,
+            cardDefense,
+            cardAmazonPrice,
+            cardCmktPrice,
+            cardCollection,
+            cardDescription,
+            cardEbayPrice,
+            cardImage,
+            cardName,
+            cardRarity,
+            cardStars,
+            cardTcgPrice,
+            cardType,
+            userId
+        );
+        const requestData = {
+            nom: cardName,
+            type: cardType,
+            description: cardDescription,
+            image: typeImageMap[cardType.toLowerCase()],
+            image_cropped: cardImage,
+            image_petite: '',
+            id_carte_konami: null,
+            attaque: cardAttack,
+            defense: cardDefense,
+            etoiles: cardStars,
+            attribut: '',
+            cardmarket_price: cardCmktPrice,
+            tcgplayer_price: cardTcgPrice,
+            ebay_price: cardEbayPrice,
+            amazon_price: cardAmazonPrice,
+            set_nom: cardCollection,
+            set_rarete: '',
+            id_user: userId,
+        };
+
+        console.log(
+            requestData.attaque,
+            requestData.defense,
+            requestData.amazon_price,
+            requestData.cardmarket_price,
+            requestData.set_nom,
+            requestData.description,
+            requestData.ebay_price,
+            requestData.image,
+            requestData.nom,
+            requestData.set_rarete,
+            requestData.etoiles,
+            requestData.tcgplayer_price,
+            requestData.type,
+            requestData.id_user
+        );
+
+        resetForm();
+        try {
+            const response = await axios.post('http://localhost:3001/cartes', requestData);
+            console.log(response.data);
+            // Gérez le succès, redirigez ou affichez un message de réussite
+        } catch (error) {
+            console.error(error);
+            // Gérez l'erreur, affichez un message d'erreur
+        }
     };
 
     const resetForm = () => {
-      setCardType('');
-      setCardName('');
-      setCardStars('');
-      setCardCollection('');
-      setCardRarity('');
-      setCardDescription('');
-      setCardCmktPrice('');
-      setCardTcgPrice('');
-      setCardEbayPrice('');
-      setCardAmazonPrice('');
-      setCardAttack('');
-      setCardDefense('');
-      setCardImage(null);
-      setUserId('');
+        setCardType('');
+        setCardName('');
+        setCardStars('');
+        setCardCollection('');
+        setCardRarity('');
+        setCardDescription('');
+        setCardCmktPrice('');
+        setCardTcgPrice('');
+        setCardEbayPrice('');
+        setCardAmazonPrice('');
+        setCardAttack('');
+        setCardDefense('');
+        setCardImage('');
+        setUserId('');
     };
-    
 
     return (
-      <div className="create-page">
-        <div className="container">
+    <div className="create-page">
+      <div className="container">
           <h1>Création d'une carte</h1>
           <div className="content-create">
-            <div className="visualisation">
-              <div className="img">
-                <p>{cardName}</p>
-                {cardStars && (
-                  <div className="stars-container">
-                    {[...Array(parseInt(cardStars))].map((_, index) => (
-                      <StarImage key={index} filled />
-                    ))}
-                  </div>
-                )}
-                <p>Collection: {cardCollection}</p>
-                <p>Rareté: {cardRarity}</p>
-                <p>Description: {cardDescription}</p>
-                <p>Cardmarket Price: {cardCmktPrice}</p>
-                <p>Tcgplayer Price: {cardTcgPrice}</p>
-                <p>Ebay Price: {cardEbayPrice}</p>
-                <p>Amazon Price: {cardAmazonPrice}</p>
-                <p>Attaque: {cardAttack}</p>
-                <p>Défense: {cardDefense}</p>
-                {cardType && (
-                  <img
-                    src={typeImageMap[cardType.toLowerCase()]}
-                    alt={`Type: ${cardType}`}
-                    className="type-image"
-                  />
-                )}
-                {cardImage && (
-                  <img
-                    src={URL.createObjectURL(cardImage)}
-                    alt="Card preview"
-                    className="card-image"
-                  />
-                )}
-                
-                </div>
+      <div className="visualisation">
+        <div className="img">
+            <p>{cardName}</p>
+            {cardStars && (
+            <div className="stars-container">
+              {[...Array(parseInt(cardStars))].map((_, index) => (
+                <StarImage key={index} filled />
+              ))}
             </div>
+            )}
+            <p>Collection: {cardCollection}</p>
+            <p>Rareté: {cardRarity}</p>
+            <p>Description: {cardDescription}</p>
+            <p>Cardmarket Price: {cardCmktPrice}</p>
+            <p>Tcgplayer Price: {cardTcgPrice}</p>
+            <p>Ebay Price: {cardEbayPrice}</p>
+            <p>Amazon Price: {cardAmazonPrice}</p>
+            <p>Attaque: {cardAttack}</p>
+            <p>Défense: {cardDefense}</p>
+            {cardType && (
+              <img
+                src={typeImageMap[cardType.toLowerCase()]}
+                alt={`Type: ${cardType}`}
+                className="type-image"
+              />
+            )}
+            {cardImage && (
+              <img
+                src={cardImage}
+                alt="Card preview"
+                className="card-image"
+              />
+            )}
+            </div>
+          </div>
             <div className="choices">
               <div className="first-box">
                 <div className="name">
@@ -220,16 +248,16 @@ export default function Create() {
                     value={cardName}
                     onChange={handleNameChange}
                   />
-                </div>
+                  </div>
                 <div className="etoiles">
-                  <select
-                    type="text"
-                    placeholder="Etoiles..."
-                    required
-                    value={cardStars}
-                    onChange={handleStarsChange}
-                    defaultValue=""
-                  >
+                    <select
+                      type="text"
+                      placeholder="Etoiles..."
+                      required
+                      value={cardStars}
+                      onChange={handleStarsChange}
+                      defaultValue=""
+                    >
                     <option value="" disabled hidden>
                       Choisir un nombre d'étoiles...
                     </option>
@@ -238,54 +266,54 @@ export default function Create() {
                         {value}
                       </option>
                     ))}
-                  </select>
-                </div>
-                <div className="type">
-                          <select
-                            name="type"
-                            id="type"
-                            required
-                            value={cardType}
-                            onChange={handleTypeChange}
-                          >
-                          <option value="" disabled hidden>
-                            Choisir un type...
-                          </option>
-                          <option value="monstre">Monstre</option>
-                          <option value="monstre_effet">Monstre Effet</option>
-                          <option value="magie">Magie</option>
-                          <option value="piege">Piège</option>
-                          <option value="fusion">Fusion</option>
-                          <option value="link">Link</option>
-                          <option value="ritual">Ritual</option>
-                          <option value="synchro">Synchro</option>
-                          <option value="xyz">Xyz</option>
+                    </select>
+                  </div>
+                    <div className="type">
+                      <select
+                        name="type"
+                        id="type"
+                        required
+                        value={cardType}
+                        onChange={handleTypeChange}
+                      >
+                      <option value="" disabled hidden>
+                        Choisir un type...
+                      </option>
+                      <option value="monstre">Monstre</option>
+                      <option value="monstre_effet">Monstre Effet</option>
+                      <option value="magie">Magie</option>
+                      <option value="piege">Piège</option>
+                      <option value="fusion">Fusion</option>
+                      <option value="link">Link</option>
+                      <option value="ritual">Ritual</option>
+                      <option value="synchro">Synchro</option>
+                      <option value="xyz">Xyz</option>
                       </select>
                     </div>
                   </div>
-              <div className="set">
-                <div className="collection">
-                  <input
-                    type="text"
-                    placeholder="Collection"
-                    value={cardCollection}
-                    onChange={handleCollectionChange}
-                  />
-                </div>
-                <div className="rarete">
-                  <input
-                    type="text"
-                    placeholder="Rareté..."
-                    required
-                    value={cardRarity}
-                    onChange={handleRarityChange}
-                  />
-                </div>
-              </div>
+                    <div className="set">
+                      <div className="collection">
+                        <input
+                          type="text"
+                          placeholder="Collection"
+                          value={cardCollection}
+                          onChange={handleCollectionChange}
+                          />
+                      </div>
+                        <div className="rarete">
+                          <input
+                            type="text"
+                            placeholder="Rareté..."
+                            required
+                            value={cardRarity}
+                            onChange={handleRarityChange}
+                          />
+                          </div>
+                        </div>    
               <div className="stats">
                 <div className="image">
                   <input
-                    type="file"
+                    type="text"
                     placeholder="Image..."
                     onChange={handleImageChange}
                   />
